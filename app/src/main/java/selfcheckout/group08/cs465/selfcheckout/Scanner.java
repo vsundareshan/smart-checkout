b43934577cb819377db9ee4e91488e088437bc2d
vsundareshan <vsundareshan@CHIC02QV06ZG8WN.sea.corp.expecn.com>
2016-11-07 22:34:16 -0600

    First commit

---
+++ app/src/main/java/selfcheckout/group08/cs465/selfcheckout/Scanner.java
@@ -0,0 +1,13 @@
+package selfcheckout.group08.cs465.selfcheckout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Scanner extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scanner);
+    }
+}
